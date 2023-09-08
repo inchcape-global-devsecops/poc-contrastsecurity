5e5ecc90aecf0cb88db65d7a9c40b2941730609b
johanntrigosinchcape <144260299+johanntrigosinchcape@users.noreply.github.com>
2023-09-08 16:28:15 +0000

    ci: Include first version

---
+++ app.js
@@ -0,0 +1,9 @@
+// Declaración de variables
+const nombre = "Juan";
+const edad = 30;
+const ciudad = "Ejemploville";
+
+// Imprimir las variables en la consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Ciudad:", ciudad);
